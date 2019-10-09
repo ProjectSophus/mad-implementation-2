24f515c7ce62cc55da6e6293ab2b3dbad9620b6b
Torstein Vik <torsteinv64@gmail.com>
2019-10-09 14:25:24 +0200

    Added InformationAgent

---
+++ core/src/main/scala/information/InformationAgent.scala
@@ -0,0 +1,15 @@
+package io.github.ProjectSophus.mad.information
+
+import io.github.ProjectSophus.mad._
+import io.github.ProjectSophus.mad.model._
+
+object InformationAgent {
+    import Information._
+    
+    def apply (info : Information, model : Model) : Model = info match {
+        case NewConcept(uid : String, name : String) => {
+            if (model.concepts contains uid) throw MADException.ConceptUIDTaken(uid)
+            model.copy(concepts = model.concepts + (uid -> Concept(name, None)))
+        }
+    }
+}
