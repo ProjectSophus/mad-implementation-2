8cc850847341694ccf1e6c0c5875ef6064579cb1
Torstein Vik <torsteinv64@gmail.com>
2019-10-10 13:13:28 +0200

    Made answer have type param

--- core/src/main/scala/questions/Answer.scala
+++ core/src/main/scala/questions/Answer.scala
@@ -2,10 +2,10 @@ package io.github.ProjectSophus.mad.questions
 
 import Answer._
 
-case class Answer (elements : (String, InputType)*)
+case class Answer (elements : (String, InputType[_])*)
 
 object Answer {
-    abstract sealed class InputType
+    abstract sealed class InputType[T]
     
-    case object string extends InputType
+    case object string extends InputType[String]
 }
