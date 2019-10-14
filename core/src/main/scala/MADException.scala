573eca42c76c6c49683f2ede14409033aa85ab69
Torstein Vik <torsteinv64@gmail.com>
2019-10-14 13:45:43 +0200

    Added machine exceptions

--- core/src/main/scala/MADException.scala
+++ core/src/main/scala/MADException.scala
@@ -5,4 +5,7 @@ abstract sealed class MADException(msg : String) extends Exception(msg)
 object MADException {
     case class ConceptUIDTaken(uid : String) extends MADException(f"The uid $uid is taken!")
     case object ConceptNameOrUIDEmpty extends MADException(f"The uid or name is empty!")
+    
+    case class MachineUIDTaken(uid : String) extends MADException(f"The uid $uid is taken!")
+    case object MachineNameOrUIDEmpty extends MADException(f"The uid or name is empty!")
 }
