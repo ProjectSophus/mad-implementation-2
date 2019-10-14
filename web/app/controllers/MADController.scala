fa323290b6617bbcc0179a9e86f22064e8293779
Torstein Vik <torsteinv64@gmail.com>
2019-10-14 13:06:52 +0200

    Added answer action

--- web/app/controllers/MADController.scala
+++ web/app/controllers/MADController.scala
@@ -8,6 +8,7 @@ import akka.stream.scaladsl._
 import io.github.ProjectSophus.mad._
 import questions._
 import model._
+import interpreter._
 
 
 @Singleton
@@ -34,6 +35,27 @@ class MADController @Inject()(cc: ControllerComponents) extends AbstractControll
         
         Ok(views.html.question(q))
         
+    }
+    
+    def answer() = Action { implicit request =>
+        val h = request.body.asFormUrlEncoded.get.apply("questionHash").head.toInt
+        
+        val questions = Question.questions(model)
+        val q = questions.find(_.hashCode() == h).get
+        
+        val data : Map[String, Any] = Map((for ((label, inputtype) <- q.answer.elements) yield label -> {
+            val element = request.body.asFormUrlEncoded.get.apply(label).head
+            
+            import Answer._
+            
+            inputtype match {
+                case Text => element
+            }
+        }) : _*)
+        
+        val info = Interpreter(q, data)
+        
+        Ok(views.html.answer(info))
     }
     
 }
