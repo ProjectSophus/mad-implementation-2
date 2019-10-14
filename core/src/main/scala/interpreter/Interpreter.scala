68b9cc82bad709e52722685eb38d6d86b1cf1c49
Torstein Vik <torsteinv64@gmail.com>
2019-10-14 13:46:40 +0200

    Added interpretation of machinequestion

--- core/src/main/scala/interpreter/Interpreter.scala
+++ core/src/main/scala/interpreter/Interpreter.scala
@@ -3,6 +3,7 @@ package io.github.ProjectSophus.mad.interpreter
 import io.github.ProjectSophus.mad._
 import questions._
 import information._
+import reference._
 
 import Question._
 import Information._
@@ -14,6 +15,15 @@ object Interpreter {
             val name = data("Name").asInstanceOf[String]
             
             Seq(NewConcept(uid, name))
+        }
+        
+        case MachineQuestion(uid, _, machinetype) => {
+            val muid = data("Unique ID").asInstanceOf[String]
+            val mname = data("Name").asInstanceOf[String]
+            
+            val (domain, codomain) = machinetype.signature.createConceptRefs(ConceptRef.BasicRef(uid))
+            
+            Seq(NewMachine(muid, mname, domain, codomain))
         }
     }
 }
