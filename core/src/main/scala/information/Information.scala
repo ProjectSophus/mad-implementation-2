c9df40d9800716b0ab4967d1ad25d9502ae97655
Torstein Vik <torsteinv64@gmail.com>
2019-10-14 13:44:27 +0200

    Added NewMachine Information

--- core/src/main/scala/information/Information.scala
+++ core/src/main/scala/information/Information.scala
@@ -1,7 +1,11 @@
 package io.github.ProjectSophus.mad.information
 
+import io.github.ProjectSophus.mad._
+import reference._
+
 abstract sealed class Information (override val toString : String)
 
 object Information {
     case class NewConcept(uid : String, name : String) extends Information(f"There is a concept named $name")
+    case class NewMachine(uid : String, name : String, domain : ConceptRef, codomain : ConceptRef) extends Information(f"There is a machine named $name that goes from $domain to $codomain")
 }
