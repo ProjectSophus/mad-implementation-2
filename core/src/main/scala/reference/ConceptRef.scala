fe1af278106f29328c4d12f3651ec0f560219d93
Torstein Vik <torsteinv64@gmail.com>
2019-10-14 13:44:53 +0200

    Added toString to ConceptRef

--- core/src/main/scala/reference/ConceptRef.scala
+++ core/src/main/scala/reference/ConceptRef.scala
@@ -1,9 +1,9 @@
 package io.github.ProjectSophus.mad.reference
 
-abstract sealed class ConceptRef
+abstract sealed class ConceptRef(override val toString : String)
 
 object ConceptRef {
-    case class BasicRef (uid : String) extends ConceptRef
+    case class BasicRef (uid : String) extends ConceptRef(uid)
     
-    case class CartesianProduct (left : ConceptRef, right : ConceptRef) extends ConceptRef
+    case class CartesianProduct (left : ConceptRef, right : ConceptRef) extends ConceptRef(f"($left) * ($right)")
 }
