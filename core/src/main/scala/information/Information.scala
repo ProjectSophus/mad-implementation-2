63ccf9856c7dc1d8418c265be649c77ba9bb4afb
Torstein Vik <torsteinv64@gmail.com>
2019-10-15 22:58:44 +0200

    Added description information

--- core/src/main/scala/information/Information.scala
+++ core/src/main/scala/information/Information.scala
@@ -8,4 +8,7 @@ abstract sealed class Information (override val toString : String)
 object Information {
     case class NewConcept(uid : String, name : String) extends Information(f"There is a concept named $name")
     case class NewMachine(uid : String, name : String, domain : ConceptRef, codomain : ConceptRef) extends Information(f"There is a machine named $name that goes from $domain to $codomain")
+    
+    case class ConceptDescription(uid : String, description : String) extends Information(f"""The concept $uid has description "$description"""")
+    case class MachineDescription(uid : String, description : String) extends Information(f"""The machine $uid has description "$description"""")
 }
