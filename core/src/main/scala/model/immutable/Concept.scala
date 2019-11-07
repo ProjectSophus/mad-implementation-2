83f16dfda96f47327a1f2de9f45c5be6f725c030
Torstein Vik <torsteinv64@gmail.com>
2019-11-07 17:22:02 +0100

    Small input org

--- core/src/main/scala/model/immutable/Concept.scala
+++ core/src/main/scala/model/immutable/Concept.scala
@@ -1,6 +1,5 @@
 package io.github.ProjectSophus.mad.model.immutable
 
 import io.github.ProjectSophus.mad._
-import reference._
 
 case class Concept (name : String, description : Option[String], relatedMachines : Seq[String])
--- core/src/main/scala/model/mutable/Concept.scala
+++ core/src/main/scala/model/mutable/Concept.scala
@@ -1,7 +1,6 @@
 package io.github.ProjectSophus.mad.model.mutable
 
 import io.github.ProjectSophus.mad._
-import reference._
 import model._
 
 case class Concept (name : String, var description : Option[String], relatedMachines : collection.mutable.Buffer[String]) {
