35d1e7ae43a7cf1473b09155a1da49d93775f44f
Torstein Vik <torsteinv64@gmail.com>
2019-11-21 17:36:46 +0100

    Small bug fix

--- core/src/main/scala/model/mutable/Object.scala
+++ core/src/main/scala/model/mutable/Object.scala
@@ -4,5 +4,5 @@ import io.github.ProjectSophus.mad._
 import model._
 
 case class Object(name : String, var description : String = null, var asConcept : Option[AsConcept] = null, var asMachine : Option[AsMachine] = null) {
-    def toObject : immutable.Object = immutable.Object(name, description, asConcept.map(_.toAsConcept), asMachine.map(_.toAsMachine))
+    def toObject : immutable.Object = immutable.Object(name, description, if (asConcept == null) null else asConcept.map(_.toAsConcept), if(asMachine == null) null else asMachine.map(_.toAsMachine))
 }
