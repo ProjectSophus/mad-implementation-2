29a56c2cb7b31144c9d1acb03c9388ff024f914f
Torstein Vik <torsteinv64@gmail.com>
2019-12-04 13:00:25 +0100

    Added some input types

--- core/src/main/scala/questions/Answer.scala
+++ core/src/main/scala/questions/Answer.scala
@@ -1,5 +1,8 @@
 package io.github.ProjectSophus.mad.questions
 
+import io.github.ProjectSophus.mad._
+import reference._
+
 import Answer._
 
 case class Answer (elements : (String, InputType[_])*)
@@ -7,6 +10,12 @@ case class Answer (elements : (String, InputType[_])*)
 object Answer {
     abstract sealed class InputType[T]
     
+    case object Label extends InputType[Unit]
+    
     case object Text extends InputType[String]
     case object LongText extends InputType[String]
+    
+    case object ConceptRef extends InputType[ConceptRef]
+    case object Object extends InputType[String]
+    case object Machine extends InputType[String]
 }
