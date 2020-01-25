00e1c8324e209061db6da030ebd6c477ad5f6395
Torstein Vik <torsteinv64@gmail.com>
2020-01-25 14:04:48 +0100

    Added compilertest

--- lang/src/test/scala/CompilerTest.scala
+++ lang/src/test/scala/CompilerTest.scala
@@ -1,17 +1,22 @@
 package io.github.ProjectSophus.mad.lang.test
 
-import io.github.ProjectSophus.mad.lang.parser._
+import io.github.ProjectSophus.mad.lang._
+import parser._
 
 import org.scalatest.FunSuite
 
-class FileParserTest extends FunSuite {
+class CompilerTest extends FunSuite {
     import AST._
     import scala.io.Source
     
-    test ("Parsing example1.mad") {
+    test ("Compiling example1.mad") {
         val source = Source.fromURL(getClass.getResource("/example1.mad"))
         val file = try source.mkString finally source.close
-        Parser.getAST(file)
+        val ast = Parser.getAST(file)
+        
+        val info = Compiler.compileASTtoInformation(ast)
+        
+        info foreach println
     }
     
 }
